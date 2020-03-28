5ea5cee9a6bfc5625cccea303a33e14766924acc
Alex Elam <aelam@grimco.com>
2020-03-28 05:37:06 -0500

    Redux Scaffold

---
+++ Code/FrontEnd/src/reducers/auth.js
@@ -0,0 +1,10 @@
+const DEFAULT_STATE = {
+  isAuthenticated: false,
+  accessToken: "",
+  error: false,
+  loading: false
+};
+
+export default (state = DEFAULT_STATE, action) => {
+  return state;
+};
