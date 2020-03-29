9bb6b3483ec62b99e238bafdd601735952073e43
Alex Elam <aelam@grimco.com>
2020-03-29 14:38:10 -0500

    Redux and IntlProvider

--- Code/FrontEnd/src/index.js
+++ Code/FrontEnd/src/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+
+// Redux Imports
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import reduxThunk from "redux-thunk";
+import reducers from "./reducers";
+
+//Internationalization and pollyfills
+import { IntlProvider } from "react-intl";
+import translationsForUsersLocale from "locales";
+import "intl";
+import "date-time-format-timezone";
+
 import App from "./pages/App";
 import * as serviceWorker from "./serviceWorker";
 import AxiosGlobalSettings from "utils/axios";
@@ -9,12 +22,28 @@ import "index.module.scss";
 
 AxiosGlobalSettings();
 
+const browserLocale = navigator.language;
+let jwtToken = localStorage.getItem("JWT_TOKEN");
+
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Provider
+    store={createStore(
+      reducers,
+      {
+        auth: {
+          isAuthenticated: jwtToken ? true : false,
+          token: jwtToken
+        }
+      },
+      applyMiddleware(reduxThunk)
+    )}
+  >
+    <IntlProvider locale={browserLocale} defaultLocale="en-US" messages={translationsForUsersLocale[browserLocale]}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </IntlProvider>
+  </Provider>,
   document.getElementById("root")
 );
 
